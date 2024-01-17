00f41e7a487637f6ad085d83dcd15fb6757b9f0e
dassolkim <kimds0926@gmail.com>
2024-01-17 15:38:45 +0900

    add auth function for resource_upload

--- ckanext/datapusher/logic/auth.py
+++ ckanext/datapusher/logic/auth.py
@@ -5,7 +5,6 @@ from typing import Any
 from ckan.types import AuthResult, Context
 import ckanext.datastore.logic.auth as auth
 
-
 def datapusher_submit(
         context: Context, data_dict: dict[str, Any]) -> AuthResult:
     return auth.datastore_auth(context, data_dict)
@@ -14,3 +13,10 @@ def datapusher_submit(
 def datapusher_status(
         context: Context, data_dict: dict[str, Any]) -> AuthResult:
     return auth.datastore_auth(context, data_dict)
+
+def datapusher_hook(context, data_dict):
+    return {'success': True}
+
+def resource_upload(context, data_dict):
+    # return p.toolkit.check_access('resource_show', context, data_dict)
+    return {'success': True}
